cb0dba6a8ff3f645da96b3bbb2997ff55a12c96e
491847583 <aimengmeng_0311@126.com>
2013-03-04 21:40:33 +0800

    测试提交

---
+++ src/com/book/main/Main.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.book.main;
+
+/**
+ * @author yinpeng
+ *
+ */
+public class Main {
+
+	public static void main(String[] args){
+		System.out.println("测试运行环境...");
+	}
+}
